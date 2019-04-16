561e0a0424b61636867f8e5377c9af1f4ac24ce3
ankitbhalala <ankit.bhalala@kuliza.com>
2019-04-16 14:49:28 +0530

    adding JSON.Stringify to env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,9 +5,11 @@ const config = {
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAHE_BUCKET,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
+
+console.log(config);
 
 firebase.initializeApp(config);
 
