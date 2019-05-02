3bcb3d4b180b7ab66b79e4113956d15dce51555f
ankitbhalala <ankit.bhalala@kuliza.com>
2019-05-02 15:30:59 +0530

    finally Done Expensify

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,28 +1,35 @@
-import React from 'react';
-import ExpenseForm from './ExpenseForm';
-import { connect } from 'react-redux';
-import { startAddExpense } from '../actions/expenses';
+import React from "react";
+import ExpenseForm from "./ExpenseForm";
+import { connect } from "react-redux";
+import { startAddExpense } from "../actions/expenses";
 
 export class AddExpensePage extends React.Component {
-  onSubmit = (expense) => {
+  onSubmit = expense => {
     // props.dispatch(addExpense(expense));
     this.props.startAddExpense(expense);
-    this.props.history.push('/');
+    this.props.history.push("/");
   };
   render() {
     return (
-      <div> 
-        <h2>Add Expense</h2>
-        <ExpenseForm
-          onSubmit={this.onSubmit}
-        />
+      <div>
+        <div className="page-header">
+          <div className="content-container">
+            <h2 className="page-header__title">Add Expense</h2>
+          </div>
+        </div>
+        <div className="content-container">
+          <ExpenseForm onSubmit={this.onSubmit} />
+        </div>
       </div>
     );
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  startAddExpense: (expense) => dispatch(startAddExpense(expense))
+const mapDispatchToProps = dispatch => ({
+  startAddExpense: expense => dispatch(startAddExpense(expense))
 });
 
-export default connect(undefined, mapDispatchToProps)(AddExpensePage);
+export default connect(
+  undefined,
+  mapDispatchToProps
+)(AddExpensePage);
---
+++ src/components/LoadingPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LoadingPage = () => (
+  <div className="loader">
+    <img className="loader__image" scr="/images/loader.gif" alt="Loading" />
+  </div>
+);
+
+export default LoadingPage;
